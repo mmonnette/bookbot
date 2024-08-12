099d6ec4cd619cc2c4a55057f368780864a0a95f
mmonnette <monnette.marshall@gmail.com>
2024-08-12 17:11:20 -0400

    working on features

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+    print(word_count(text))
+
+#ingest and print book
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+#count words
+def word_count(file):
+    words = file.split()
+    return len(words)
+
+main()
